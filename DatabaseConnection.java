d6d44019e25a855f36a19e9b2f18c6e03b22d827
Aravind.P <aravindsaamy615@gmail.com>
2024-09-04 12:21:05 +0530

    Update DatabaseConnection.java

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -1,9 +1,9 @@
 import java.sql.*;
 public class DatabaseConnection {
 	
-	private static final String URL = "jdbc:mysql://localhost:3306/student_db"; //use your db_name
+	private static final String URL = "jdbc:mysql://localhost:3306/______"; //use your db_name
     private static final String USER = "root";
-    private static final String PASSWORD = "Mysql,29"; // Use your MySQL password
+    private static final String PASSWORD = " "; // Use your MySQL password
 
     public static Connection getConnection() throws Exception {
         return DriverManager.getConnection(URL, USER, PASSWORD);
